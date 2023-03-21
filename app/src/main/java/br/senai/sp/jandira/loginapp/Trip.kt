b8fbea322bb94cbd976a25bf2f56fe9a005d23b9
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-03-21 11:23:37 -0300

    teste

--- app/src/main/java/br/senai/sp/jandira/loginapp/Trip.kt
+++ app/src/main/java/br/senai/sp/jandira/loginapp/Trip.kt
@@ -3,13 +3,17 @@ package br.senai.sp.jandira.loginapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.loginapp.ui.theme.LoginAppTheme
 
 class Trip : ComponentActivity() {
@@ -32,6 +36,13 @@ fun Greeting3(name: String) {
 @Composable
 fun TripApp() {
     LoginAppTheme {
-        
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Card(modifier = Modifier.fillMaxWidth()
+                    .height(360.dp)) {
+                    Image(painter = painterResource(id = R.drawable.background), contentDescription = "")
+                }
+            }
+        }
     }
 }
