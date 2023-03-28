9a5549cc0db7b1db0663ba7d36fa1518c228a27c
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-03-28 11:28:20 -0300

    criando tabela banco

--- app/src/main/java/br/senai/sp/jandira/loginapp/model/User.kt
+++ app/src/main/java/br/senai/sp/jandira/loginapp/model/User.kt
@@ -1,13 +1,15 @@
 package br.senai.sp.jandira.loginapp.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "table_user")
 data class User(
-    var id: Long = 0,
-    var userName: String = "",
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "user_name") var userName: String = "",
     var email: String = "",
     var password: String = "",
     var phone: String ="",
-    var isOver18: Boolean = false
+    @ColumnInfo(name = "is_over_18")var isOver18: Boolean = false
 )
