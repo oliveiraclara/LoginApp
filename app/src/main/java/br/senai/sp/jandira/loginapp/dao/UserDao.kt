86d3c9686fefd3f2be5d6fd9ce02e412152c5011
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-04-04 10:11:17 -0300

    criando o dao

---
+++ app/src/main/java/br/senai/sp/jandira/loginapp/dao/UserDao.kt
@@ -0,0 +1,7 @@
+package br.senai.sp.jandira.loginapp.dao
+
+import androidx.room.Dao
+
+@Dao
+interface UserDao  {
+}
---
+++ app/src/main/java/br/senai/sp/jandira/loginapp/repository/UserRepository.kt
@@ -0,0 +1,4 @@
+package br.senai.sp.jandira.loginapp.repository
+
+class UserRepository {
+}
